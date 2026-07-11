655f51dbc41e8c52a7eab0e7f9c679e1baf833df
Eloi Aguirre <iole12345ag@gmail.com>
2026-07-11 19:05:14 +0200

    uploaded exercices

---
+++ Day-1/Exercice2.py
@@ -0,0 +1,9 @@
+my_name= "Eloi"
+my_surname="Aguirre i Godayol"
+my_country="Spain"
+enjoy="I enjoy 30 days of python ;D"
+
+print(my_name)
+print(my_surname)
+print(my_country)
+print(enjoy)
---
+++ Day-1/Exercice3.py
@@ -0,0 +1,9 @@
+print("10 is an integer (int)")
+print("9.8 is a float")
+print("3.14 is a float")
+print("4 - 4j is a complex")
+print("['Asabeneh', 'Python', 'Finland'] is a list")
+print("My name is a string")
+print("My family name is a string")
+print("My country is a string")
+

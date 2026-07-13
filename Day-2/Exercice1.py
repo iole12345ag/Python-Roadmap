0226f0704f7401c0f02c7a017e33ec8cd829d725
Eloi Aguirre <iole12345ag@gmail.com>
2026-07-13 22:08:52 +0200

    Add files via upload

---
+++ Day-2/Exercice1.py
@@ -0,0 +1,47 @@
+import math
+print("Day 2: 30 Days of python programming")
+
+First_name="Daniel"
+Last_name="Monge"
+Full_name="Daniel Monge"
+Country="Spain"
+City="Barcelona"
+Age=18
+Year=2000
+Is_married=False
+Is_true=True
+Is_light_on=False
+name,last_name="Daniel","Monge"
+
+type(First_name)
+type(Last_name)
+type(Full_name)
+type(Country)
+type(City)
+type(Age)
+type(Year)
+type(Is_married)
+type(Is_true)
+type(Is_true)
+
+print(len(First_name))
+print(len(Last_name))
+
+num_one=1
+num_two=2
+total = num_one + num_two
+substraction=num_one - num_two
+multiplication=num_one * num_two
+division=num_one / num_two
+moodulus=num_one % num_two
+to_the_power=num_one **num_two
+floor_division = num_one // num_two
+
+
+radius = float(input("Say the radius of a circle: "))
+Area= math.pi * radius**2
+Perimeter = 2*math.pi*radius
+print(f"The Perimeter is {Perimeter} and the Area is {Area}")
+
+
+
